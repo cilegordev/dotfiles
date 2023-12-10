cf9227c7c112bf8c6767454a77a57418fe70d46d
cilegordev <deputyalectro@gmail.com>
2023-12-10 07:02:51 +0700

    modified:   config/hypr/configs/Keybinds.conf modified:   config/hypr/hyprland.conf modified:   config/hypr/scripts/KeyHints.sh modified:   README.md modified:   config/hypr/configs/Keybinds.conf

--- config/hypr/scripts/KeyHints.sh
+++ config/hypr/scripts/KeyHints.sh
@@ -10,22 +10,24 @@ yad --width=1000 --height=1000 \
 --column=Command: \
 --timeout=60 \
 --timeout-indicator=bottom \
-" + ENTER" "Open Terminal" "Command Prompt" \
-" + SPACE" "Chage Wallpaper" "Swww" \
-" + R" "Refresh Configure" "Waybar" \
+" + ~" "Open Terminal" "Command Prompt" \
 " + Q" "Minimize" "Hide waybar" \
 " + W" "Close" "Kill Active Programs" \
 " + E" "Fullscreen" "Full Screen Programs" \
+" + R" "Refresh Configure" "Waybar" \
+" + T" "Open Cava" "Cava" \
+" + Y" "Open Pipes.sh" "Test Colors" \
 " + A" "Open File Manager" "Files" \
 " + S" "Open Web Browser" "Browsers" \
 " + D" "Open Menu" "Show Menus" \
 " + Z" "Unfocuse Window" "Toggle Floating" \
 " + X" "Open Task Manager" "Task Manager" \
 " + C" "Open Text Editor" "Edit" \
+" + M" "Record My Desktop" "Recorder" \
+" + ENTER" "Open Terminal" "Command Prompt" \
+" + SPACE" "Chage Wallpaper" "Swww" \
 " + 1-0" "Move Workspace" "Workspace" \
 " + LEFT, RIGHT, UP, DOWN" "Move Window" "Workspace" \
 " + CLICK" "Toggle Floating" "Move Window" \
-"wf-recorder -a -f capture.mp4" "RECORD MY DESKTOP" "TERMINAL" \
-"pipes.sh" "TEST COLOR TERMINAL" "TERMINAL" \
 "" "" "" \
 "AUTO CLOSE" "AFTER READ" "60 SECOND" \
