088eafe21b8a541d310976ee4c8006bac3810503
cilegordev <deputyalectro@gmail.com>
2023-12-30 15:26:33 +0700

    Correction

--- config/hypr/scripts/airplanemode.sh
+++ config/hypr/scripts/airplanemode.sh
@@ -4,7 +4,7 @@ directoriy="$HOME/.config/dunst/icons"
 
 wifistatus=$(nmcli radio wifi)
 if [ "$wifistatus" == "enabled" ]; then
-    notify-send -t 1000 'airplane mode: inactive' -i "$directoriy/airplane-mode.svg"
+    notify-send -t 1000 'airplane mode: inactive' -i "$directoriy/airplane-mode-disabled-symbolic.svg"
 else
-    notify-send -t 1000 'airplane mode: active' -i "$directoriy/airplane-mode-disabled-symbolic.svg"
+    notify-send -t 1000 'airplane mode: active' -i "$directoriy/airplane-mode.svg"
 fi
